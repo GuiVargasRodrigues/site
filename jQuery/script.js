31b816ac984df3ab636a78fc288c55f4ffab5c61
Guilherme <guilhermevargasrodrigues8@gmail.com>
2024-07-03 21:16:18 -0300

    trs

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll', function() {
     let scrollPosition = window.pageYOffset;
-    let parallaxElements = document.querySelectorAll('.parallax-container:before');
+    let parallaxElements = document.querySelectorAll('.parallax-container');
 
-    parallaxElements.forEach(function(elem, _parallax) {
+    parallaxElements.forEach(function(elem) {
         let depth = 0.5;
         let movement = -(scrollPosition * depth);
         elem.style.transform = `translateY(${movement}px)`;
